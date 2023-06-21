9c98fc82907c509409b79195ff4f1cfa18d184fb
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-06-20 18:49:22 -0700

    completed survey

--- app.py
+++ app.py
@@ -9,44 +9,57 @@ app.config['SECRET_KEY'] = "never-tell!"
 debug =DebugToolbarExtension(app)
 
 RESPONSES_KEY= "response"
-user_responses = []
 
 @app.route('/')
 def survey_home():
     
     return render_template('start_survey.html', survey = survey)
 
+
 @app.route("/start", methods=["POST"])
 def start_survey():
+    """redirects the user to the first question of the survey"""
+    session[RESPONSES_KEY] = []
 
     return redirect("/questions/0")
 
+
 @app.route("/questions/<int:qid>")
 def show_questions(qid):
+    """Displays the current questions"""
+    responses = session.get(RESPONSES_KEY)
 
-    if len(user_responses) == len(survey.questions):
+    if responses is None:
+        """ redirects the user back to the home page if they attempt to access questions before clicking start"""
+        return redirect("/")
+
+    if len(responses) == len(survey.questions):
         """once all questions have been answered, send them to the completed page """
         return redirect("/complete")
     
-    if len(user_responses) != qid:
+    if len(responses) != qid:
         """a message will display if the user attempts to skip ahead or enter an invalid question number"""
         flash(f"Invalid question number: {qid}")
-        return redirect(f"/questions/{len(user_responses)}")
+        return redirect(f"/questions/{len(responses)}")
 
     question = survey.questions[qid]
     return render_template("question.html", question_num=qid, question=question)
 
 @app.route("/answer", methods=["POST"])
 def survey_response():
-    choice = request.form['answer']
-    user_responses.append(choice)
+    """adds the user responses to the response list"""
 
-    if len(user_responses) == len(survey.questions):
-        """confirms all questions have been answered and sends them to the completion page"""
+    choice = request.form['answer']
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if len(responses) == len(survey.questions):
+        """confirms that all the questions have been answered and send the user to the completion page"""
         return redirect("/complete")
 
     else:
-        return redirect(f"/questions/{len(user_responses)}")
+        return redirect(f"/questions/{len(responses)}")
     
 @app.route("/complete")
 def completed():
