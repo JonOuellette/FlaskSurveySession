6066c84b721d6fcb4d40da2286e2c1bbbb21976e
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-06-20 17:44:32 -0700

    attempting to route to questions

--- app.py
+++ app.py
@@ -5,11 +5,23 @@ from surveys import satisfaction_survey as survey
 app = Flask(__name__)
 
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
+app.config['SECRET_KEY'] = "never-tell!"
 debug =DebugToolbarExtension(app)
 
 responses = []
 
 @app.route('/')
 def survey_home():
+    
     return render_template('start_survey.html', survey = survey)
+
+@app.route("/start")
+def start_survey():
+
+    return redirect("/questions/0")
+
+@app.route("/questions/<int:qid>")
+def view_questions(qid):
+
+    question = survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=question)
