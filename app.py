6c54f3bcb2044d3614281bfbdcd7d55113cbdbd3
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-06-20 18:39:15 -0700

    Survey completed w/o Session

--- app.py
+++ app.py
@@ -23,7 +23,15 @@ def start_survey():
 
 @app.route("/questions/<int:qid>")
 def show_questions(qid):
+
+    if len(user_responses) == len(survey.questions):
+        """once all questions have been answered, send them to the completed page """
+        return redirect("/complete")
     
+    if len(user_responses) != qid:
+        """a message will display if the user attempts to skip ahead or enter an invalid question number"""
+        flash(f"Invalid question number: {qid}")
+        return redirect(f"/questions/{len(user_responses)}")
 
     question = survey.questions[qid]
     return render_template("question.html", question_num=qid, question=question)
@@ -34,7 +42,13 @@ def survey_response():
     user_responses.append(choice)
 
     if len(user_responses) == len(survey.questions):
+        """confirms all questions have been answered and sends them to the completion page"""
         return redirect("/complete")
 
     else:
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(user_responses)}")
+    
+@app.route("/complete")
+def completed():
+    """Survey has been completed, display completion page"""
+    return render_template("completed.html")
