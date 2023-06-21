90352e9b52d218c449aabe1501dc028770c50783
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-06-20 18:26:18 -0700

    Not working after first answer

--- app.py
+++ app.py
@@ -8,20 +8,33 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = "never-tell!"
 debug =DebugToolbarExtension(app)
 
-responses = []
+RESPONSES_KEY= "response"
+user_responses = []
 
 @app.route('/')
 def survey_home():
     
     return render_template('start_survey.html', survey = survey)
 
-@app.route("/start")
+@app.route("/start", methods=["POST"])
 def start_survey():
 
     return redirect("/questions/0")
 
 @app.route("/questions/<int:qid>")
-def view_questions(qid):
+def show_questions(qid):
+    
 
     question = survey.questions[qid]
     return render_template("question.html", question_num=qid, question=question)
+
+@app.route("/answer", methods=["POST"])
+def survey_response():
+    choice = request.form['answer']
+    user_responses.append(choice)
+
+    if len(user_responses) == len(survey.questions):
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
